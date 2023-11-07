0b1c1c1185ce78992ef0449c6e3c7ce96dccbdf0
sadsad33 <bonhyuk33@naver.com>
2023-11-07 23:00:18 +0900

    Lock On # Animator - 록온시 이동 모션 추가
    # PlayerLocomotion
    - 록온 상태에도 달리기나 구르기 동작시 움직이던 방향대로 행동할 수 있게 한다.
    - 록온 상태에서 달리기나 구르기 동작입력이 없다면 플레이어가 타겟을 바라보게 회전값을 설정한다.
    - 록온 상태의 경우 플레이어 오브젝트가 바라보는 방향이 고정되기 때문에 이동 상태에서 animator BlendTree의 수평, 수직이동값을 모두 사용해 이동하도록 한다.
    
    # CameraHandler
    - 적이 장애물로 시야에서 가려져있다면 록온되지 않도록 한다
    - 록온시 카메라 높낮이 변경

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace sg {
     public class PlayerLocomotion : MonoBehaviour {
+        CameraHandler cameraHandler;
         Transform cameraObject;
         InputHandler inputHandler;
         PlayerManager playerManager;
@@ -41,6 +42,9 @@ namespace sg {
         [SerializeField]
         float fallingSpeed = 80;
 
+        private void Awake() {
+            cameraHandler = FindObjectOfType<CameraHandler>();
+        }
 
         void Start() {
             playerManager = GetComponent<PlayerManager>();
@@ -63,27 +67,52 @@ namespace sg {
 
         // 캐릭터 회전
         private void HandleRotation(float delta) {
-            // 바라볼 방향
-            Vector3 targetDir = Vector3.zero;
-            // WASD 방향키 값을 반영한다.
-            targetDir = cameraObject.forward * inputHandler.vertical;
-            targetDir += cameraObject.right * inputHandler.horizontal;
-            targetDir.Normalize();
-            // 위아래로는 회전하지 않을 것
-            targetDir.y = 0;
+            if (inputHandler.lockOnFlag) {
+                // 록온을 해도 달리거나 구를때는, 이동하던 방향으로 행동
+                if (inputHandler.sprintFlag || inputHandler.rollFlag) {
+                    Vector3 targetDirection = Vector3.zero;
+                    targetDirection = cameraHandler.cameraTransform.forward * inputHandler.vertical;
+                    targetDirection += cameraHandler.cameraTransform.right * inputHandler.horizontal;
+                    targetDirection.Normalize();
+                    targetDirection.y = 0;
+                    if (targetDirection == Vector3.zero) {
+                        targetDirection = transform.forward;
+                    }
+                    Quaternion tr = Quaternion.LookRotation(targetDirection);
+                    Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr, rotationSpeed * Time.deltaTime);
+                    transform.rotation = targetRotation;
+                } else {
+                    Vector3 rotationDirection = moveDirection;
+                    rotationDirection = cameraHandler.currentLockOnTarget.position - transform.position;
+                    rotationDirection.y = 0;
+                    rotationDirection.Normalize();
+                    Quaternion tr = Quaternion.LookRotation(rotationDirection);
+                    Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr, rotationSpeed * Time.deltaTime);
+                    transform.rotation = targetRotation;
+                }
+            } else {
+                // 바라볼 방향
+                Vector3 targetDir = Vector3.zero;
+                // WASD 방향키 값을 반영한다.
+                targetDir = cameraObject.forward * inputHandler.vertical;
+                targetDir += cameraObject.right * inputHandler.horizontal;
+                targetDir.Normalize();
+                // 위아래로는 회전하지 않을 것
+                targetDir.y = 0;
 
-            // 방향 조작이 없다면 캐릭터의 현재좌표에서 정면을 바라봄
-            if (targetDir == Vector3.zero)
-                targetDir = myTransform.forward;
+                // 방향 조작이 없다면 캐릭터의 현재좌표에서 정면을 바라봄
+                if (targetDir == Vector3.zero)
+                    targetDir = myTransform.forward;
 
-            float moveOverride = inputHandler.moveAmount;
+                float moveOverride = inputHandler.moveAmount;
 
-            // 스크립트에서 회전 처리를 다루는 경우 Quaternion 클래스와 이 클래스의 함수를 사용하여 회전 값을 만들고 수정해야 한다.
-            // 일부의 경우 스크립트에서 오일러 각을 사용하는 것이 더 좋다. 이 경우 각을 변수로 유지하고 회전에 오일러 각으로 적용하는 데만 사용해야 하고 궁극적으로 Quaterion 으로 저장되어야 한다.
-            float rs = rotationSpeed;
-            Quaternion tr = Quaternion.LookRotation(targetDir); // 회전
-            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta); // 회전이 부드럽게 이어지도록한다
-            myTransform.rotation = targetRotation; // 회전값을 Quaternion으로 저장한다.
+                // 스크립트에서 회전 처리를 다루는 경우 Quaternion 클래스와 이 클래스의 함수를 사용하여 회전 값을 만들고 수정해야 한다.
+                // 일부의 경우 스크립트에서 오일러 각을 사용하는 것이 더 좋다. 이 경우 각을 변수로 유지하고 회전에 오일러 각으로 적용하는 데만 사용해야 하고 궁극적으로 Quaterion 으로 저장되어야 한다.
+                float rs = rotationSpeed;
+                Quaternion tr = Quaternion.LookRotation(targetDir); // 회전
+                Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta); // 회전이 부드럽게 이어지도록한다
+                myTransform.rotation = targetRotation; // 회전값을 Quaternion으로 저장한다.
+            }
         }
 
         // 캐릭터 이동
@@ -123,7 +152,12 @@ namespace sg {
             Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
             rigidbody.velocity = projectedVelocity;
 
-            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0, playerManager.isSprinting);
+            // 록온 상태의경우 수평이동 입력값과 수직이동 입력값을 모두 사용한다.
+            if (inputHandler.lockOnFlag && !inputHandler.sprintFlag) { 
+                animatorHandler.UpdateAnimatorValues(inputHandler.vertical, inputHandler.horizontal, playerManager.isSprinting);
+            } else { // 아닐경우 정면방향으로 움직이면 되므로 수직이동값만 사용
+                animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0, playerManager.isSprinting);
+            }
 
             if (animatorHandler.canRotate)
                 HandleRotation(delta);
