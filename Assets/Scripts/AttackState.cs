b4211ac5a2d647201d474c18aebf2d9f7c75b3f1
sadsad33 <bonhyuk33@naver.com>
2023-11-10 00:48:09 +0900

    AI # PursueTargetState - 목표 추적 - 공격 사거리내에 타겟이 들어오면 Combat Stance State가 된다 - 타겟이 공격사거리 밖으로 나가면 Pursue Target State 유지 - 적 오브젝트의 상태에 따라 회전
    # CombatStanceState
    - 공격 사거리 확인
    - 공격 대상 주위를 배회
    - 공격 사거리 내에 들어가면 Attack State가 된다
    - 공격후 대기상태라면 Combat Stance State로 돌아오고 타겟 주위를 배회
    - 타겟이 공격사거리 밖으로 도망가면 Pursue Target State가 된다
    
    # AttackState
    - attack score 값에 따라 수행가능한 공격중 하나를 선택한다
    - 선택된 공격이 각도나 거리때문에 불가능하다면 새로운 공격을 선택
    - 공격이 가능하다면 이동을 멈춘 후 공격
    - 공격 딜레이 시간을 세팅
    - Combat Stance State로 돌아간다
    
    # EnemyAnimatorManager
    - Rigidbody를 EnemyManager에서 호출하도록 변경했기에 EnemyManager 인스턴스 호출
    
    # EnemyLocomotionManager + EnemyManager
    - 상태를 사용하기위한 여러 변경

--- Assets/Scripts/AttackState.cs
+++ Assets/Scripts/AttackState.cs
@@ -4,13 +4,81 @@ using UnityEngine;
 
 namespace sg {
     public class AttackState : State {
+        public CombatStanceState combatStanceState;
+        public EnemyAttackActions[] enemyAttacks;
+        public EnemyAttackActions currentAttack;
         public override State Tick(EnemyManager enemyManager, EnemyStats enemyStats, EnemyAnimatorManager enemyAnimatorManager) {
             // attack score 변수에 따라 수행가능한 공격들중 하나를 선택한다
             // 선택된 공격이 각도나 거리에 의해 불가능해 진다면 새로운 공격을 선택
             // 공격이 가능하다면 이동을 멈춘후 공격
             // 공격 딜레이 시간을 세팅한다.
-            // Combat Stance State로 돌아감 
-            return this;
+            // Combat Stance State로 돌아감
+
+            //Vector3 targetDirection = enemyManager.currentTarget.transform.position - transform.position;
+            
+            // 대상 공격
+            if (enemyManager.isPerformingAction) return combatStanceState;
+
+            if (currentAttack != null) {
+                // 만약 타겟이 공격하기에 너무 가까이 있다면 새로운 공격을 선택한다.
+                if (enemyManager.distanceFromTarget < currentAttack.minimumDistanceNeededToAttack) {
+                    return this;
+                } else if (enemyManager.distanceFromTarget < currentAttack.maximumDistanceNeededToAttack) {
+                    if (enemyManager.viewableAngle <= currentAttack.maximumAttackAngle && enemyManager.viewableAngle >= currentAttack.minimumAttackAngle) {
+                        if (enemyManager.currentRecoveryTime <= 0 && !enemyManager.isPerformingAction) {
+                            enemyAnimatorManager.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+                            enemyAnimatorManager.anim.SetFloat("Horizontal", 0, 0.1f, Time.deltaTime);
+                            enemyAnimatorManager.PlayTargetAnimation(currentAttack.actionAnimation, true);
+                            enemyManager.isPerformingAction = true;
+                            enemyManager.currentRecoveryTime = currentAttack.recoveryTime;
+                            currentAttack = null;
+                            return combatStanceState;
+                        }
+                    }
+                }
+            } else {
+                GetNewAttack(enemyManager);
+            }
+
+            return combatStanceState;
+        }
+
+        // 공격 선택
+        private void GetNewAttack(EnemyManager enemyManager) {
+            Vector3 targetDirection = enemyManager.currentTarget.transform.position - transform.position;
+            float viewableAngle = Vector3.Angle(targetDirection, transform.forward);
+            enemyManager.distanceFromTarget = Vector3.Distance(enemyManager.currentTarget.transform.position, transform.position);
+
+            int maxScore = 0;
+            for (int i = 0; i < enemyAttacks.Length; i++) {
+                EnemyAttackActions enemyAttackAction = enemyAttacks[i];
+
+                if (enemyManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack &&
+                    enemyManager.distanceFromTarget >= enemyAttackAction.minimumDistanceNeededToAttack) { // 현재 목표가 공격 사거리 내에 있고
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle && viewableAngle >= enemyAttackAction.minimumAttackAngle) { // 공격 가능한 시야각내에 있다면
+                        maxScore += enemyAttackAction.attackScore;
+                    }
+                }
+            }
+
+            int randomValue = Random.Range(0, maxScore);
+            int temporaryScore = 0;
+            for (int i = 0; i < enemyAttacks.Length; i++) {
+                EnemyAttackActions enemyAttackAction = enemyAttacks[i];
+
+                if (enemyManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack &&
+                    enemyManager.distanceFromTarget >= enemyAttackAction.minimumDistanceNeededToAttack) {
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle && viewableAngle >= enemyAttackAction.minimumAttackAngle) {
+                        if (currentAttack != null) return; // 이미 공격중이라면
+                        temporaryScore += enemyAttackAction.attackScore;
+
+                        if (temporaryScore > randomValue) {
+                            currentAttack = enemyAttackAction;
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
--- Assets/Scripts/CombatStanceState.cs
+++ Assets/Scripts/CombatStanceState.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 namespace sg {
     public class CombatStanceState : State {
+        public AttackState attackState;
+        public PursueTargetState pursueTargetState;
         public override State Tick(EnemyManager enemyManager, EnemyStats enemyStats, EnemyAnimatorManager enemyAnimatorManager) {
             // 공격 사거리 확인
             // 공격 대상 주위에서 걷거나 빙글빙글 돈다
             // 공격 사거리 내에 들어가면 Attack State가 된다.
             // 공격후 딜레이 상태라면 Combat Stance State로 돌아오고 타겟 주위를 배회
             // 만약 타겟이 공격 사거리 밖으로 도망가버리면 Pursue Target State가 됨.
-            return this;
+
+            enemyManager.distanceFromTarget = Vector3.Distance(enemyManager.currentTarget.transform.position, enemyManager.transform.position);
+            if (enemyManager.currentRecoveryTime <= 0 && enemyManager.distanceFromTarget <= enemyManager.maximumAttackRange) {
+                return attackState;
+            } else if (enemyManager.distanceFromTarget > enemyManager.maximumAttackRange) {
+                return pursueTargetState;
+            } else
+                return this;
         }
     }
 }
