74d8327b1f0bd383d7b099ea05496d6b8d6e5d83
sadsad33 <bonhyuk33@naver.com>
2023-12-16 22:03:10 +0900

    캐릭터 모델 교체

---
+++ Assets/Scripts/Items/EquipmentItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace sg {
+    public class EquipmentItem : Item {
+        [Header("Defense Bonus")]
+        public float physicalDefense;
+        // 마력 방어
+        // 화염 방어
+        // 신성 방어
+        // 어둠 방어
+        // 벼락 방어 ...
+    }
+}
