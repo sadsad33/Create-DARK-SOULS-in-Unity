e39c8bc7ec53a9f6717a09f9b0205705f47d7346
sadsad33 <bonhyuk33@naver.com>
2023-12-19 22:16:23 +0900

    방어력 적용 # CharacterStats - 갑옷 부분별 물리 감쇄율(%)들을 저장할 변수 - TakeDamage 함수에서 각 파츠별 감쇄율에 따른 최종 데미지 계산, 플레이어에게 적용
    # PlayerStats, EnemyStats
    - 데미지를 계산을 부모 클래스의 메서드로 이동
    
    # PlayerEquipmentManager
    - EquipmentItem 에는 방어력을 저장하는 변수가 존재
    - 방어구를 장착하게되면 각 파츠별로 방어력을 적용

--- Assets/Scripts/Managers/CharacterStats.cs
+++ Assets/Scripts/Managers/CharacterStats.cs
@@ -18,10 +18,28 @@ namespace sg {
 
         public int soulCount = 0;
 
+        [Header("Armor Absorptions")]
+        public float physicalDamageAbsorptionHead;
+        public float physicalDamageAbsorptionBody;
+        public float physicalDamageAbsorptionLegs;
+        public float physicalDamageAbsorptionHands;
+
         public bool isDead;
 
-        public virtual void TakeDamage(float damage, string damageAnimation = "Damage") {
-        
+        public virtual void TakeDamage(float physicalDamage, string damageAnimation = "Damage") {
+            if (isDead) return;
+
+            float totalPhysicalDamageAbsorption = 1 - (1 - physicalDamageAbsorptionHead / 100) * (1 - physicalDamageAbsorptionBody / 100) * (1 - physicalDamageAbsorptionLegs / 100) * (1 - physicalDamageAbsorptionHands / 100);
+            physicalDamage -= (physicalDamage * totalPhysicalDamageAbsorption);
+            Debug.Log("Total Physical Damage Absorption is " + totalPhysicalDamageAbsorption + "%");
+            float finalDamage = physicalDamage;
+            currentHealth -= finalDamage;
+            Debug.Log("Total Damage Dealt is " + finalDamage);
+
+            if (currentHealth <= 0) {
+                currentHealth = 0;
+                isDead = true;
+            }
         }
     }
 }
