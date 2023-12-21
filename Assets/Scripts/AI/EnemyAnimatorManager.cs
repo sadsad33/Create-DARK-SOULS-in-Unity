cebdc69182f6309192e834fa94ad0b7ca04409ba
sadsad33 <bonhyuk33@naver.com>
2023-12-21 23:17:44 +0900

    적 회전 # RotateTowardsTargetState - Enemy 오브젝트가 플레이어를 향해 회전을 수행하는 상태 - Enemy 오브젝트의 정면 방향과 Enemy 오브젝트로부터 플레이어로의 방향 사이의 각도에 따라 알맞은 회전 애니메이션을 수행한다.
    # AnimatorManager
    - 루트 모션을 통한 회전 애니메이션을 사용하는데 canRotate 매개변수의 상태와 상관없이 회전해야 하므로 함수를 따로 만들어 사용한다.
    
    # Animator
    - isRotatingWithMotionStatus
    - canRotate와 별개로 루트 모션을 사용한 회전 애니메이션을 수행할지를 판단할 .bool Parameter를 추가한다.
    
    # ResetAnimatorBool
    - 애니메이션 동작이 끝나고 Empty 상태로 돌아왔을때 초기화 해줄 bool Parameter에 isRotatingWithMotionStatus를 추가해준다.
    
    # CharacterManager
    - Animator의 isRotatingWithRootMotion Parameter의 상태를 EnemyManager에서 받아 사용하기 위해 변수를 추가한다.
    
    # EnemyManager
    - Update 메서드 내에서 Animator의 isRotatingWithRootMotion의 상태를 받아온다.
    
    # EnemyAniamtorManager
    - OnAnimatorMove 함수내에서 Animator 의 Parameter인 isRotatingWithRootMotion이 true 상태라면 Enemy 오브젝트의 회전값에 변화를 준다.

--- Assets/Scripts/AI/EnemyAnimatorManager.cs
+++ Assets/Scripts/AI/EnemyAnimatorManager.cs
@@ -12,13 +12,20 @@ namespace sg {
             enemyStats = GetComponentInParent<EnemyStats>();
         }
 
+
+        // RootMotion이 적용된 애니메이션이 재생중에 RootPosition이 이동되면 호출
+        // OnAnimatorMove 함수를 구현하게 되면 Animator 컴포넌트의 ApplyRootMotion이 HandledByScript가 되어 OnAnimatorMove 함수내에서 이동 관련 로직을 구현해 주어야 함
         private void OnAnimatorMove() {
             float delta = Time.deltaTime;
-            enemyManager.enemyRigidbody.drag = 0;
+            enemyManager.enemyRigidbody.drag = 0; // 미끄러짐 방지
             Vector3 deltaPosition = anim.deltaPosition;
             deltaPosition.y = 0;
             Vector3 velocity = deltaPosition / delta;
             enemyManager.enemyRigidbody.velocity = velocity;
+
+            if (enemyManager.isRotatingWithRootMotion) {
+                enemyManager.transform.rotation *= anim.deltaRotation;
+            }
         }
 
         public override void TakeCriticalDamageAnimationEvent() {
--- Assets/Scripts/ResetAnimatorBool.cs
+++ Assets/Scripts/ResetAnimatorBool.cs
@@ -16,11 +16,14 @@ public class ResetAnimatorBool : StateMachineBehaviour {
     public string isInvulnerableBool = "isInvulnerable";
     public bool isInvulnerableStatus = false;
 
+    public string isRotatingWithRootMotion = "isRotatingWithRootMotion";
+    public bool isRotatingWithRootMotionStatus = false;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         animator.SetBool(isInteractingBool, isInteractingStatus);
         animator.SetBool(isFiringSpellBool, isFiringSpellStatus);
         animator.SetBool(canRotateBool, canRotateBoolStatus);
         animator.SetBool(isInvulnerableBool, isInvulnerableStatus);
+        animator.SetBool(isRotatingWithRootMotion, isRotatingWithRootMotionStatus);
         //Debug.Log("애니메이션 종료");
     }
 }
