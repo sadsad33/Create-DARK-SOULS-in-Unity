a497e81bc9434de47bd647a1b7272a2182eca9d5
sadsad33 <bonhyuk33@naver.com>
2024-07-16 22:50:07 +0900

    세이브 - 월드에 스폰된 아이템들의 상태 저장 - 월드에 스폰된 아이템들은 두가지의 식별 변수를 사용한다 - 월드에 스폰된 아이템들을 서로 구분하기 위한 식별 번호와 플레이어가 해당 아이템을 이미 회수했는지 안했는지의 여부를 나타낼 식별 bool 변수를 가짐
    # SerializableDictionary
    - 일반화 컬렉션 사용
    - 이 클래스는 딕셔너리 클래스와, ISerializationCallbackReceiver 인터페이스를 상속
    - key, value 값에 사용할 데이터들을 list 로 가지고 있으며 직렬화 하기 바로 전에 key 값들과 value 값들을 리스트에 기록
    - 역직렬화 바로 이후에 만약 key 값들의 개수와 value 값들의 개수가 다르다면 에러 메시지를 출력하고 현재 key 값의 수 만큼 딕셔너리에 추가한다.
    
    # CharacterSaveData
    - 월드로 부터 회수할 수 있는 아이템들의 목록을 기록할 딕셔너리를 가짐
    - 딕셔너리의 모든 pair 항목들의 value 값을 통해 해당 아이템을 플레이어가 회수했는지 안했는지 알수 있음
    
    # ItemPickUp
    - 씬이 로드되면 해당 씬의 스폰 아이템들은 현재 캐릭터의 세이브 데이터에서 자기 자신의 식별번호 pair 항목의 value 값이 false 인지 검사
    - 만약 value 값이 false 면, 플레이어는 해당 아이템을 회수한 적이 없는 것이므로 pair의 bool 값을 false로 기록한후 딕셔너리에 추가
    - 만약 이미 회수된 아이템이라면 비활성화 한다.
    - 상호작용시 딕셔너리에서 해당 아이템의 식별번호를 찾은후 항목을 삭제, pair의 bool 값을 true로 해서 다시 딕셔너리에 추가한다.

--- Assets/Scripts/Items/Interactables/Interactable.cs
+++ Assets/Scripts/Items/Interactables/Interactable.cs
@@ -12,6 +12,11 @@ namespace SoulsLike {
             Gizmos.DrawWireSphere(transform.position, radius);
         }
 
+        protected virtual void Awake() {
+        }
+
+        protected virtual void Start() {
+        }
 
         // 다른 클래스에서 사용할때 override해서 사용할 수 있도록 virtual 형으로 만든다.
         public virtual void Interact(PlayerManager playerManager) {
--- Assets/Scripts/Items/ItemPickUp.cs
+++ Assets/Scripts/Items/ItemPickUp.cs
@@ -5,10 +5,50 @@ using UnityEngine.UI;
 
 namespace SoulsLike {
     public class ItemPickUp : Interactable {
-        //public WeaponItem weapon;
+        [Header("Item Information")]
+        [SerializeField] int itemPickUpID; // 월드에 스폰될 아이템들의 식별 번호
+        [SerializeField] bool hasBeenLooted; // 플레이어가 회수했는지 안했는지 여부
         public Item item;
+
+        protected override void Awake() {
+            base.Awake();
+        }
+
+        protected override void Start() {
+            base.Start();
+
+            // 만약 현재 캐릭터 세이브 데이터의
+            // 월드에서 회수한 아이템 목록에 이 아이템이 없다면
+            // 이 아이템을 회수한 적이 없는 것
+            // 따라서 회수 되지 않았다고 기록해야 함
+            if (!WorldSaveGameManager.instance.currentCharacterSaveData.itemsInWorld.ContainsKey(itemPickUpID)) {
+                WorldSaveGameManager.instance.currentCharacterSaveData.itemsInWorld.Add(itemPickUpID, false);
+            }
+
+            hasBeenLooted = WorldSaveGameManager.instance.currentCharacterSaveData.itemsInWorld[itemPickUpID];
+
+            if (hasBeenLooted) {
+                gameObject.SetActive(false);
+            }
+        }
+        
         public override void Interact(PlayerManager playerManager) {
             base.Interact(playerManager); // 부모의 Interact함수를 호출한다.
+
+            // 캐릭터 데이터에 이 아이템이 플레이어에게 회수되었는지를 알려 만약 이미 회수된 아이템이라면 다시 월드에 스폰되지 않도록 함
+
+            // 만약 현재 캐릭터 세이브 데이터의 게임 월드에서 회수한 아이템 목록에 이 스폰 아이템의 식별 번호가 존재한다면
+            if (WorldSaveGameManager.instance.currentCharacterSaveData.itemsInWorld.ContainsKey(itemPickUpID)) {
+                // 해당 식별번호를 key 값으로 하는 항목을 제거
+                WorldSaveGameManager.instance.currentCharacterSaveData.itemsInWorld.Remove(itemPickUpID);
+            }
+
+            // 해당 식별번호를 key 값으로 하는 항목의 value에 true 를 입력하여 다시 추가
+            WorldSaveGameManager.instance.currentCharacterSaveData.itemsInWorld.Add(itemPickUpID, true);
+
+            hasBeenLooted = true;
+
+            // 플레이어의 인벤토리에 아이템을 전달한다
             PickUpItem(playerManager);
         }
         
