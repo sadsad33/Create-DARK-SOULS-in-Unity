f1733dae75182fa4d773f1fd75e0eb67c58994ca
sadsad33 <bonhyuk33@naver.com>
2024-07-22 11:02:50 +0900

    Netcode 방어구 동기화
    # CharacterNetworkManager
    - 현재 캐릭터의 머리, 몸, 손, 다리 갑옷의 식별번호를 저장할 네트워크 변수
    
    # PlayerNetworkManager
    - 갑옷을 변경하는 기능이 플레이어 한테만 있기 때문에 실질적인 메서드를 여기서 구현
    - 각 부위별로 이 캐릭터의 주인이 현재 클라이언트가 아니라면 인수로 넘겨받은 식별번호로 갑옷을 교체
    
    # PlayerEquipmentManager
    - 플레이어가 모든 부위의 갑옷을 장착할 때
    - 해당 부위가 비어있지 않다면 네트워크 변수들에 현재 갑옷의 식별번호를 전달
    - 해당 부위가 비어있다면 네트워크 변수에 -1 전달
    
    # PlayerManager
    - 네트워크의 부위별 갑옷 변수들의 OnValueChanged 이벤트에 부위별 갑옷읠 변경하는 함수를 핸들러로 연결
    - 네트워크에 접속했을 당시의 다른 클라이언트들 상태를 바로 반영해주기 위해 각 부위별 갑옷 교체 함수들을 호출

--- Assets/Scripts/PlayerNetworkManager.cs
+++ Assets/Scripts/PlayerNetworkManager.cs
@@ -5,6 +5,56 @@ using Unity.Netcode;
 
 namespace SoulsLike {
     public class PlayerNetworkManager : CharacterNetworkManager {
+        PlayerManager player;
 
+        protected override void Awake() {
+            base.Awake();
+            player = GetComponent<PlayerManager>();
+        }
+        // 갑옷 변경
+        public void OnHeadEquipmentChange(int oldEquipmentID, int newEquipmentID) {
+            if (player.IsOwner) return;
+            EquipmentItem equipment = WorldItemDatabase.instance.GetEquipmentItemByID(newEquipmentID);
+
+            if (equipment != null) {
+                player.characterInventoryManager.currentHelmetEquipment = equipment as HelmetEquipment;
+            } else {
+                player.characterInventoryManager.currentHelmetEquipment = null;
+            }
+            player.playerEquipmentManager.EquipAllEquipmentModels();
+        }
+        public void OnTorsoEquipmentChange(int oldEquipmentID, int newEquipmentID) {
+            if (player.IsOwner) return;
+            EquipmentItem equipment = WorldItemDatabase.instance.GetEquipmentItemByID(newEquipmentID);
+
+            if (equipment != null) {
+                player.characterInventoryManager.currentTorsoEquipment = equipment as TorsoEquipment;
+            } else {
+                player.characterInventoryManager.currentTorsoEquipment = null;
+            }
+            player.playerEquipmentManager.EquipAllEquipmentModels();
+        }
+        public void OnGuntletEquipmentChange(int oldEquipmentID, int newEquipmentID) {
+            if (player.IsOwner) return;
+            EquipmentItem equipment = WorldItemDatabase.instance.GetEquipmentItemByID(newEquipmentID);
+
+            if (equipment != null) {
+                player.characterInventoryManager.currentGuntletEquipment = equipment as GuntletEquipment;
+            } else {
+                player.characterInventoryManager.currentGuntletEquipment = null;
+            }
+            player.playerEquipmentManager.EquipAllEquipmentModels();
+        }
+        public void OnLegEquipmentChange(int oldEquipmentID, int newEquipmentID) {
+            if (player.IsOwner) return;
+            EquipmentItem equipment = WorldItemDatabase.instance.GetEquipmentItemByID(newEquipmentID);
+
+            if (equipment != null) {
+                player.characterInventoryManager.currentLegEquipment = equipment as LegEquipment;
+            } else {
+                player.characterInventoryManager.currentLegEquipment = null;
+            }
+            player.playerEquipmentManager.EquipAllEquipmentModels();
+        }
     }
 }
